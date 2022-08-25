405ef1858dc44f900f96795cd6740d832846cee8
mmillenaar <matiasmillenaar95@gmail.com>
2022-08-25 13:38:30 -0300

    Added Handlebars view engine
    Also corrected PUT method and organized code

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+let emptyList
 let productList = [
     {
         "title": "Escuadra",
@@ -30,70 +31,73 @@ const save = (product) => {
         newId = lastId + 1
     }
     productList.push({...product, id: newId})
-    return JSON.stringify(productList[productList.length -1])
+    return productList
 }
-
 const getById = (id) => {
-    const filteredProduct = productList.filter(object => object.id === id)
-    if (filteredProduct.length === 0) {
+    let productIndex = productList.findIndex(element => element.id == id)
+    if (productIndex < 0) {
         return { error: 'Product not found' }
     }
     else {
-        return filteredProduct[0]
+        return productIndex
     }
 }
-
 const deleteById = (id) => {
     const filteredList = productList.filter(object => object.id !== id)
     if (filteredList.length == productList.length) {
         return { error: 'Product not found' }
     } else {
         productList = filteredList
-        return `Product successfully deleted, your updated product list is: ${JSON.stringify(productList)}`
+        return `Product successfully deleted, your updated product consists of: ${JSON.stringify(productList)}`
     }
 }
-
 const deleteAll = () => {
     productList = []
     return { success: 'All products have been deleted' }
 }
 
 
-router.get('/', (req, res) => {
-    res.send(productList)
-})
+router
+    .route('/')
+    .get((req, res) => {
+        if (productList.length === 0) {
+            emptyList = true
+        }
+        res.render('data', {productList, emptyList})
+    })
+    .post((req, res) => {
+        const { title, price, thumbnail } = req.body
+        let newProduct = {
+            title: title,
+            price: price,
+            thumbnail: thumbnail,
+        }
+        save(newProduct)
+        res.render('data', { productList, emptyList: false })
+    });
 
-router.get('/:id', (req, res) => {
-    let requestedProduct = getById(parseInt(req.params.id))
-    res.send(requestedProduct)
-})
-
-router.post('/', (req, res) => {
-    const { title, price, thumbnail } = req.body
-    let newProduct = {
-        title: title,
-        price: price,
-        thumbnail: thumbnail,
-    }
-    let newProductWithId = save(newProduct)
-    res.send(newProductWithId)
-})
-
-router.put('/:id', (req, res) => {
-    let requestedProduct = getById(parseInt(req.params.id))
-    const { title, price, thumbnail } = req.body
-    let modifiedProduct = {
-        title: title,
-        price: price,
-        thumbnail: thumbnail,
-        id: requestedProduct.id
-    }
-    res.send(`Product ${JSON.stringify(modifiedProduct)} successfully saved`)
-})
-
-router.delete('/:id', (req, res) => {
-    let newProductList = deleteById(parseInt(req.params.id))
-    res.send(newProductList)
-})
+router
+    .route('/:id')
+    .get((req, res) => {
+        let requestedProductIndex = getById(parseInt(req.params.id))
+        res.send(productList[requestedProductIndex])
+    })
+    .put((req, res) => {
+        let requestedProductIndex = getById(parseInt(req.params.id))
+        const newTitle = req.body.title
+        const newPrice = req.body.price
+        const newThumbnail = req.body.thumbnail
+        productList[requestedProductIndex] = {
+            title: newTitle && newTitle,
+            price: newPrice && newPrice,
+            thumbnail: newThumbnail && newThumbnail,
+            id: productList[requestedProductIndex].id
+        }
+        res.send(`Changes saved. Your updated product list consists of: ${JSON.stringify(productList)}`)
+    })
+    .delete((req, res) => {
+        let newProductList = deleteById(parseInt(req.params.id))
+        res.send(newProductList)
+    })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const app = express()
-const router = require('./routes/products')
+const products = require('./routes/products')
+const { engine } = require('express-handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-app.use('/api/products', router)
+app.use('/api/products', products)
 
-router.get('/', (req, res) => {
-    res.send('todo ok')
-})
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
 
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
