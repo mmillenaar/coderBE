e2766a2fd159ad56d54cfa1351e3178ebf908c75
mmillenaar <matiasmillenaar95@gmail.com>
2022-12-06 20:30:54 -0300

    Artillery test and logger optimization

--- src/config/logger.config.js
+++ src/config/logger.config.js
@@ -1,23 +1,20 @@
 import pino, { multistream } from 'pino'
+import pretty from 'pino-pretty'
 import path from 'path'
 
 const streams = [
     {
-        level: 'info',
-        stream: process.stdout
+        stream: pretty()
     },
     {
-        level: 'error'
-        , stream: pino.destination(path.resolve('./logs/error.log'))
+        level: 'error',
+        stream: pino.destination(path.resolve('./logs/error.log'))
     }
 ]
 
 const logger = pino(
     {
         level: 'info',
-        transport: {
-            target: 'pino-pretty'
-        },
     },
     multistream(streams),
 )
